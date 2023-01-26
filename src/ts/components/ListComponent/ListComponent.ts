a78ff9d91b7ccdcb6957765edb28b3b500f514ce
Inge Heeringa <inge.heeringa@gmail.com>
2023-01-26 22:18:06 +0100

    Fix dynamic rendering of list info pending and watched series

--- src/ts/components/ListComponent/ListComponent.ts
+++ src/ts/components/ListComponent/ListComponent.ts
@@ -1,22 +1,28 @@
-import series from "../../series/series";
-import { type SeriesStructure, type Series } from "../../types/types";
+import { type Series } from "../../types/types";
 import Component from "../Component/Component";
 import CardComponent from "../CardComponent/CardComponent";
 
 class ListComponent extends Component {
   title: string;
   series: Series;
+  listInfo: string;
 
-  constructor(parentElement: Element, title: string, series: Series) {
+  constructor(
+    parentElement: Element,
+    title: string,
+    listInfo: string,
+    series: Series
+  ) {
     super(parentElement, "list", "series");
     this.title = title;
+    this.listInfo = listInfo;
     this.series = series;
   }
 
   render() {
     super.render();
     this.element.innerHTML = `<h3 class="list__title">${this.title}</h3>
-        <span class="list__info">You have 4 series pending to watch</span>
+        <span class="list__info">${this.listInfo}</span>
         <!--<span class="list__info">Congrats! You've watched all your series</span>-->
         <ul class="series">
         
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,10 +1,14 @@
 import HeaderComponent from "./components/HeaderComponent/HeaderComponent";
 import ListContainerComponent from "./components/ListContainerComponent/ListContainerComponent";
+import series from "./series/series";
 
 const appContainer = document.querySelector(".container")!;
 
 const headerComponent = new HeaderComponent(appContainer);
-const seriesListContainerComponent = new ListContainerComponent(appContainer);
+const seriesListContainerComponent = new ListContainerComponent(
+  appContainer,
+  series
+);
 
 headerComponent.render();
 seriesListContainerComponent.render();
